017cfe4b115bb144184139031c87bc3300aa6a5f
lkrishan <lkrishan@LIN22005819.corp.capgemini.com>
2021-03-30 12:15:49 +0530

    First commit

---
+++ src/main/java/com/capgemini/DBSOperation/DbsOperationApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.DBSOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/DBSOperation/DbsOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.DBSOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
