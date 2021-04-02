d40c838fdfa66a192510551239622f983cb52f9d
Salonigavade <gavade.saloni01@gmail.com>
2021-04-02 12:08:49 +0530

    AssociatePersonal and AssociateProfessional Controller

--- src/main/java/com/capgemini/service/OperationService.java
+++ src/main/java/com/capgemini/service/OperationService.java
@@ -1,21 +1,24 @@
 package com.capgemini.service;
 
+import java.util.List;
+
+import com.capgemini.entity.AssociatePersonal;
 import com.capgemini.entity.AssociateProf;
+import com.capgemini.exception.AssociateException;
 import com.capgemini.exception.OperationException;
 
 public interface OperationService {
 
 
 
-	public Integer addAssociateDetails(AssociateProf operationTeam) throws OperationException;
-
-	
+	public Integer addAssociateProfDetails( Integer cgGroupId,AssociateProf associateProf) throws OperationException;
 
 
-	//public List<OperationTeam> getAllAssociateDetails() throws OperationException;
+	public List<AssociateProf> getAllAssociateProfDetails() throws OperationException;
 
+	public AssociateProf getAssociateProfById(Integer sNo) throws OperationException;
 
-	public AssociateProf updateAssociateDetails(AssociateProf operationTeam) throws OperationException;
+	public AssociateProf updateAssociateProfDetails(AssociateProf associateProf) throws OperationException;
 
 
 }
