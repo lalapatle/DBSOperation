c73bfc649497b5cd01af874b228984d1d4483452
lkrishan <lkrishan@LIN22005819.corp.capgemini.com>
2021-04-02 12:10:18 +0530

    cc

--- src/main/java/com/capgemini/controller/OperationTeamController.java
+++ src/main/java/com/capgemini/controller/OperationTeamController.java
@@ -1,8 +1,11 @@
 package com.capgemini.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.service.OperationService;
 
 @RestController
 @CrossOrigin("*")
