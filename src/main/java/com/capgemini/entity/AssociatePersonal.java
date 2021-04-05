44a96653db3c7ca767105f8b33267a09a9c0ebb6
sahyerra <sahyerra@LIN37001206.corp.capgemini.com>
2021-04-05 15:10:13 +0530

    sahithi

--- src/main/java/com/capgemini/entity/AssociatePersonal.java
+++ src/main/java/com/capgemini/entity/AssociatePersonal.java
@@ -159,20 +159,380 @@ public class AssociatePersonal {
 	@Column(name="spoc")
 	private String spoc;
 
+	public AssociatePersonal(Integer cgGroupId, String associateFullName, String gender, String cgUserName,
+			String cgMailId, String region, String practice, String designation, String cgSupervisor,
+			String cgDBSAccountSupervisor, String dbsClientLead, String tower, String shortTower,
+			String reasonResignation, String associateLocation, Date dateOfJoiningDBSAccount, Date dbsBillableStartDate,
+			String bankId, String dbsMailId, String primarySkill, String overallExperienceBeforeJoiningCg,
+			String sowNumber, String mandatoryTraining, String onboardingDocs, String panCard, String passport,
+			Date passportExpiryDate, Date dateOfBirth, String foreignEmploymentExperience, String contact,
+			String emergencyContact, String temporaryAddress, String permanentAddress, String csbForm,
+			String cgLaptopSlno, String dbsLaptopSlno, Date dateOfLaptopTaken, Date dateOfLaptopReturn, String spoc) {
+		super();
+		this.cgGroupId = cgGroupId;
+		this.associateFullName = associateFullName;
+		this.gender = gender;
+		this.cgUserName = cgUserName;
+		this.cgMailId = cgMailId;
+		this.region = region;
+		this.practice = practice;
+		this.designation = designation;
+		this.cgSupervisor = cgSupervisor;
+		this.cgDBSAccountSupervisor = cgDBSAccountSupervisor;
+		this.dbsClientLead = dbsClientLead;
+		this.tower = tower;
+		this.shortTower = shortTower;
+		this.reasonResignation = reasonResignation;
+		this.associateLocation = associateLocation;
+		this.dateOfJoiningDBSAccount = dateOfJoiningDBSAccount;
+		this.dbsBillableStartDate = dbsBillableStartDate;
+		this.bankId = bankId;
+		this.dbsMailId = dbsMailId;
+		this.primarySkill = primarySkill;
+		this.overallExperienceBeforeJoiningCg = overallExperienceBeforeJoiningCg;
+		this.sowNumber = sowNumber;
+		this.mandatoryTraining = mandatoryTraining;
+		this.onboardingDocs = onboardingDocs;
+		this.panCard = panCard;
+		this.passport = passport;
+		this.passportExpiryDate = passportExpiryDate;
+		this.dateOfBirth = dateOfBirth;
+		this.foreignEmploymentExperience = foreignEmploymentExperience;
+		this.contact = contact;
+		this.emergencyContact = emergencyContact;
+		this.temporaryAddress = temporaryAddress;
+		this.permanentAddress = permanentAddress;
+		this.csbForm = csbForm;
+		this.cgLaptopSlno = cgLaptopSlno;
+		this.dbsLaptopSlno = dbsLaptopSlno;
+		this.dateOfLaptopTaken = dateOfLaptopTaken;
+		this.dateOfLaptopReturn = dateOfLaptopReturn;
+		this.spoc = spoc;
+	}
 
+	public Integer getCgGroupId() {
+		return cgGroupId;
+	}
 
+	public void setCgGroupId(Integer cgGroupId) {
+		this.cgGroupId = cgGroupId;
+	}
 
+	public String getAssociateFullName() {
+		return associateFullName;
+	}
 
+	public void setAssociateFullName(String associateFullName) {
+		this.associateFullName = associateFullName;
+	}
 
+	public String getGender() {
+		return gender;
+	}
 
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
+	public String getCgUserName() {
+		return cgUserName;
+	}
 
+	public void setCgUserName(String cgUserName) {
+		this.cgUserName = cgUserName;
+	}
 
+	public String getCgMailId() {
+		return cgMailId;
+	}
 
+	public void setCgMailId(String cgMailId) {
+		this.cgMailId = cgMailId;
+	}
 
+	public String getRegion() {
+		return region;
+	}
 
+	public void setRegion(String region) {
+		this.region = region;
+	}
 
+	public String getPractice() {
+		return practice;
+	}
 
+	public void setPractice(String practice) {
+		this.practice = practice;
+	}
 
+	public String getDesignation() {
+		return designation;
+	}
 
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getCgSupervisor() {
+		return cgSupervisor;
+	}
+
+	public void setCgSupervisor(String cgSupervisor) {
+		this.cgSupervisor = cgSupervisor;
+	}
+
+	public String getCgDBSAccountSupervisor() {
+		return cgDBSAccountSupervisor;
+	}
+
+	public void setCgDBSAccountSupervisor(String cgDBSAccountSupervisor) {
+		this.cgDBSAccountSupervisor = cgDBSAccountSupervisor;
+	}
+
+	public String getDbsClientLead() {
+		return dbsClientLead;
+	}
+
+	public void setDbsClientLead(String dbsClientLead) {
+		this.dbsClientLead = dbsClientLead;
+	}
+
+	public String getTower() {
+		return tower;
+	}
+
+	public void setTower(String tower) {
+		this.tower = tower;
+	}
+
+	public String getShortTower() {
+		return shortTower;
+	}
+
+	public void setShortTower(String shortTower) {
+		this.shortTower = shortTower;
+	}
+
+	public String getReasonResignation() {
+		return reasonResignation;
+	}
+
+	public void setReasonResignation(String reasonResignation) {
+		this.reasonResignation = reasonResignation;
+	}
+
+	public String getAssociateLocation() {
+		return associateLocation;
+	}
+
+	public void setAssociateLocation(String associateLocation) {
+		this.associateLocation = associateLocation;
+	}
+
+	public Date getDateOfJoiningDBSAccount() {
+		return dateOfJoiningDBSAccount;
+	}
+
+	public void setDateOfJoiningDBSAccount(Date dateOfJoiningDBSAccount) {
+		this.dateOfJoiningDBSAccount = dateOfJoiningDBSAccount;
+	}
+
+	public Date getDbsBillableStartDate() {
+		return dbsBillableStartDate;
+	}
+
+	public void setDbsBillableStartDate(Date dbsBillableStartDate) {
+		this.dbsBillableStartDate = dbsBillableStartDate;
+	}
+
+	public String getBankId() {
+		return bankId;
+	}
+
+	public void setBankId(String bankId) {
+		this.bankId = bankId;
+	}
+
+	public String getDbsMailId() {
+		return dbsMailId;
+	}
+
+	public void setDbsMailId(String dbsMailId) {
+		this.dbsMailId = dbsMailId;
+	}
+
+	public String getPrimarySkill() {
+		return primarySkill;
+	}
+
+	public void setPrimarySkill(String primarySkill) {
+		this.primarySkill = primarySkill;
+	}
+
+	public String getOverallExperienceBeforeJoiningCg() {
+		return overallExperienceBeforeJoiningCg;
+	}
+
+	public void setOverallExperienceBeforeJoiningCg(String overallExperienceBeforeJoiningCg) {
+		this.overallExperienceBeforeJoiningCg = overallExperienceBeforeJoiningCg;
+	}
+
+	public String getSowNumber() {
+		return sowNumber;
+	}
+
+	public void setSowNumber(String sowNumber) {
+		this.sowNumber = sowNumber;
+	}
+
+	public String getMandatoryTraining() {
+		return mandatoryTraining;
+	}
+
+	public void setMandatoryTraining(String mandatoryTraining) {
+		this.mandatoryTraining = mandatoryTraining;
+	}
+
+	public String getOnboardingDocs() {
+		return onboardingDocs;
+	}
+
+	public void setOnboardingDocs(String onboardingDocs) {
+		this.onboardingDocs = onboardingDocs;
+	}
+
+	public String getPanCard() {
+		return panCard;
+	}
+
+	public void setPanCard(String panCard) {
+		this.panCard = panCard;
+	}
+
+	public String getPassport() {
+		return passport;
+	}
+
+	public void setPassport(String passport) {
+		this.passport = passport;
+	}
+
+	public Date getPassportExpiryDate() {
+		return passportExpiryDate;
+	}
+
+	public void setPassportExpiryDate(Date passportExpiryDate) {
+		this.passportExpiryDate = passportExpiryDate;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getForeignEmploymentExperience() {
+		return foreignEmploymentExperience;
+	}
+
+	public void setForeignEmploymentExperience(String foreignEmploymentExperience) {
+		this.foreignEmploymentExperience = foreignEmploymentExperience;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getEmergencyContact() {
+		return emergencyContact;
+	}
+
+	public void setEmergencyContact(String emergencyContact) {
+		this.emergencyContact = emergencyContact;
+	}
+
+	public String getTemporaryAddress() {
+		return temporaryAddress;
+	}
+
+	public void setTemporaryAddress(String temporaryAddress) {
+		this.temporaryAddress = temporaryAddress;
+	}
+
+	public String getPermanentAddress() {
+		return permanentAddress;
+	}
+
+	public void setPermanentAddress(String permanentAddress) {
+		this.permanentAddress = permanentAddress;
+	}
+
+	public String getCsbForm() {
+		return csbForm;
+	}
+
+	public void setCsbForm(String csbForm) {
+		this.csbForm = csbForm;
+	}
+
+	public String getCgLaptopSlno() {
+		return cgLaptopSlno;
+	}
+
+	public void setCgLaptopSlno(String cgLaptopSlno) {
+		this.cgLaptopSlno = cgLaptopSlno;
+	}
+
+	public String getDbsLaptopSlno() {
+		return dbsLaptopSlno;
+	}
+
+	public void setDbsLaptopSlno(String dbsLaptopSlno) {
+		this.dbsLaptopSlno = dbsLaptopSlno;
+	}
+
+	public Date getDateOfLaptopTaken() {
+		return dateOfLaptopTaken;
+	}
+
+	public void setDateOfLaptopTaken(Date dateOfLaptopTaken) {
+		this.dateOfLaptopTaken = dateOfLaptopTaken;
+	}
+
+	public Date getDateOfLaptopReturn() {
+		return dateOfLaptopReturn;
+	}
+
+	public void setDateOfLaptopReturn(Date dateOfLaptopReturn) {
+		this.dateOfLaptopReturn = dateOfLaptopReturn;
+	}
+
+	public String getSpoc() {
+		return spoc;
+	}
+
+	public void setSpoc(String spoc) {
+		this.spoc = spoc;
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
